f475f8e0c7235bf6cdaf7e58776e3783f732e242
Oleksandr Krokhmal <alexkrokhmall@gmail.com>
2016-11-13 23:11:41 +0200

    Adding HomeTaks2 (2 variants)

---
+++ Hometask_2 - FizzBuzz-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_2___FizzBuzz_2
+{
+    class SecondTry
+    {
+        static void Main(string[] args)
+        {
+
+            int counter = 1;
+            int divideBy15Result = 0;
+
+            while (counter <= 100)
+            {
+                divideBy15Result = ModuleBy15(counter);
+
+                switch (divideBy15Result)
+                {
+                    case 3:
+                    case 6:
+                    case 9:
+                    case 12:
+                        Console.WriteLine("Fizz");
+                        break;
+                    case 5:
+                    case 10:
+                        Console.WriteLine("Buzz");
+                        break;
+                    case 0:
+                        Console.WriteLine("FizzBuzz");
+                        break;
+                    default:
+                        Console.WriteLine(counter);
+                        break;
+                }
+
+                counter++;
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int ModuleBy15(int incomingValueToBeDivided)
+        {
+            int moduleBy15Result = incomingValueToBeDivided % 15;
+            return moduleBy15Result;
+        }
+
+    }
+
+}
---
+++ Homework_2-FizzBuzz/Homework_2-FizzBuzz/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "***** Homework_2_FizzBuzz *****";
+
+            Console.WriteLine("The programm writes integers from 1 to 100.\n- for multiples of 3 prints Fizz\n- for the multiples of 5 prints Buzz\n- for multiples of both 3 and 5 prints FizzBuzz\n");
+
+            CheckIfDividedByThreeOrFiveOrFifteen();
+
+            Console.ReadLine();
+
+        }
+
+        static void CheckIfDividedByThreeOrFiveOrFifteen()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        WriteFizzBuzzToConsole(i);
+                    }
+                    else
+                    {
+                        WriteFizzToConsole(i);
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    WriteBuzzToConsole(i);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void WriteFizzToConsole(int valueToWrite)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Fizz");
+            Console.ResetColor();
+        }
+
+        static void WriteBuzzToConsole(int valueToWrite)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Buzz");
+            Console.ResetColor();
+        }
+
+        static void WriteFizzBuzzToConsole(int valueToWrite)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("FizzBuzz");
+            Console.ResetColor();
+        }
+    }
+}
